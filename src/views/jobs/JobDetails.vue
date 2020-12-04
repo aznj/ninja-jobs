d33b9566c6b7467b77e5d4fed7eeaece3916e8f0
Akmal Jamal <akmal.jamal@photobookworldwide.com>
2020-12-04 17:39:05 +0800

    Add JobDetails page.

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,16 +1,28 @@
 <template>
-  <p>Details page</p>
-  <p>id is {{ id }}</p>
+  <div v-if="job">
+    <p>{{ job.title }}</p>
+    <p>id is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading</p>
+  </div>
 </template>
 
 <script>
 export default {
   props: ['id'],
-  /*data() {
+  data() {
     return {
-      id: this.$route.params.id
+      job: null
     }
-  }*/
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs/' + this.id)
+        .then((res) => res.json())
+        .then(data => this.job = data)
+        .catch(err => console.log(err.message))
+  }
 }
 </script>
 
